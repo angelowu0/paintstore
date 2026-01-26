17e0f627fe1c4fbba72a385954b0692934800877
Angelo Wu <angeloWu@student.unsw.edu.au>
2026-01-26 19:32:00 +1100

    test

--- SampleProject/Program.cs
+++ SampleProject/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+// define a string called paint name, and assign value 'Dulux' to it
+string paintName = "Dulux";
+
+// define an int called num of paints, and assign value 100 to it
+int numPaints = 100;
+
+// define define a proper type for price of the paint, assign 19.99
+decimal pricePaint = 19.99m;
+
+Console.WriteLine("{0} {1} {2}",paintName, numPaints.ToString(), pricePaint.ToString());
